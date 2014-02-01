d91753394e132132b191bdd5b07d270c02e30164
Eduardo Javier Echeverria Alvarado <echevemaster@gmail.com>
2014-02-01 06:23:37 -0430

    Adding setup.py

---
+++ fcms/metadata.py
@@ -0,0 +1,15 @@
+""":mod: Floss Community Management System.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'fcms'
+project = "fcms"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Floss Community Management System'
+authors = ['Eduardo Echeverria']
+authors_string = ', '.join(authors)
+emails = ['echevemaster@gmail.com']
+license = 'MIT'
+copyright = '2014 ' + authors_string
+url = 'http://echevemaster.org/'
